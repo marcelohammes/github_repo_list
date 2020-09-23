a1f7055d7aafc8c447af05db7c5949bc76c7bbdf
Marcelo Hammes <marcelo.hammes@rarolabs.com.br>
2020-09-23 20:40:02 -0300

    Using BaseViewController in SceneDelegate

--- GithubRepoList/GithubRepoList/SceneDelegate.swift
+++ GithubRepoList/GithubRepoList/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
-        let viewController = UIViewController()
+        let viewController = BaseViewController<UIView>()
 
         let navigation = UINavigationController(rootViewController: viewController)
         window.rootViewController = navigation
